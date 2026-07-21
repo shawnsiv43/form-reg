10509278214091d488fa04af3f626407e799de38
shawnsiv43 <shawnsiv43@gmail.com>
2026-07-21 19:20:18 -0400

    Clean up

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
-from flask import Flask,request,jsonify,render_template
+from flask import Flask,request,jsonify,render_template,redirect,url_for
 import sqlite3
-import webbrowser
-conn=sqlite3.connect("C:\\Users\\hi_sh\\formregsave.db")
-cursor=conn.cursor()
+#import webbrowser
+
+def get_db():
+    conn=sqlite3.connect("C:\\databases\\formregsave.db")
+    conn.row_factory=sqlite3.Row
+    return conn
 
 latest_sql_err=None
 
 app=Flask(__name__)
 @app.route("/regform")
 def dis():
-    return render_template("index.html")
+    return render_template("form.html")
 @app.route("/api/reg")
 def diss():
     global latest_sql_err
-    conn=sqlite3.connect("C:\\Users\\hi_sh\\formregsave.db")
+    conn=sqlite3.connect("C:\\databases\\formregsave.db")
     cursor=conn.cursor()
     uName=request.args.get("uName")
     print("uname is",uName)
@@ -21,32 +24,28 @@ def diss():
     print("eName is",eName)
     passName=request.args.get("passName")
     print("passName is",passName)
-    cpassName=request.args.get("cpassName")
-    print("cpassName is",cpassName)
     ageName=request.args.get("ageName")
     print("ageName is",ageName)
     try:
-        information=(uName,eName,ageName,passName,cpassName)
-        insert_query='''Insert into info(username,email,age,password,confirmpassword)
-        Values(?,?,?,?,?)'''
+        information=(uName,eName,ageName,passName)
+        insert_query='''Insert into info(username,email,age,password)
+        Values(?,?,?,?)'''
 
         cursor.execute(insert_query,information)
         print('db saved')
 
         conn.commit()
-
-        conn.close()
         latest_sql_err=None
-        return jsonify({"regMsg":f"Your Registration has been {uName}, {eName}, {passName}, {cpassName}, {ageName}"})
+        return jsonify({"regMsg":f"Your Registration has been {uName}, {eName}, {passName}, {ageName}"})
     except sqlite3.IntegrityError as e:
         latest_sql_err=str(e)
 
         return jsonify({"regMsg":f"Registration failure. Integrity constraint on {uName}","error":latest_sql_err})
     finally:
         conn.close()
-@app.route("/success")
-def disss():
-    global latest_sql_err
+# @app.route("/success")
+# def disss():
+#     global latest_sql_err
     isSuccess=request.args.get("isSuccess")
     # html_content=f"""<h1>{isSuccess}</h1>
     # <a href="http://127.0.0.1:5000/regform">Register a change in the fields</a>"""
@@ -60,13 +59,100 @@ def disss():
     # return jsonify({"status":"success","error":None})
     #webbrowser.open("C:\\Users\\hi_sh\\python-coding-git\\form-reg\\templates\\success.html")
     #return "<H1>success</H1>"
-    
-    print("success",isSuccess)
+
+    # print("success",isSuccess)
+    # return render_template(
+    #     "success.html",
+    #     isSuccess=isSuccess
+    # )
+# @app.route("/search")
+# def dissss():
+#     return render_template(
+#         "search.html"
+#     )
+# @app.route("/search_submit")
+# def disssss():
+#     print("happy")
+#     conn=sqlite3.connect("C:\\Users\\hi_sh\\formregsave.db")
+#     cursor=conn.cursor()
+#     input=request.args.get("input")
+#     query="SELECT * from info where username=?"
+#     rows=cursor.execute(query,(input,)).fetchall()
+#     conn.commit()
+#     string=''
+#     for row in rows:
+#         string=f"""Username: {row[0]}
+#         Email: {row[1]}
+#         Age: {str(row[2])}
+#         Password: {str(row[3])}
+#         Confirm Passord: {row[4]}"""
+#     conn.close()
+#     return jsonify({'lll':string})
+
+@app.route("/delete/<string:input2>",methods=['GET','POST'])
+def delete(input2):
+    conn=sqlite3.connect("C:\\databases\\formregsave.db")
+    cursor=conn.cursor()
+    #input2=request.args.get("input2")
+    print(input2)
+    query2="""Delete From info
+    where username=?"""
+    cursor.execute(query2,(input2,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for("list_all_users"))
+
+@app.route("/update/<string:username>", methods=['GET','POST'])
+def update(username):
+    conn=get_db()
+    print("Shawn")
+    print(request.method)
+    print(username)
+
+
+
+    print("Mmarcus")
+    if request.method=="POST":
+
+        data=request.get_json()
+        print(data)
+        print(data["email"])
+        print(data["age"])
+        print(data["password"])
+        print(username)
+        try:
+            sql_query="""Update info
+            Set email=?,
+            age=?,
+            password=?,
+            Where username=?"""
+            conn.execute(sql_query,(data["email"],data["age"],data["password"],username))
+            conn.commit()
+            return jsonify({"happy":"success"})
+        finally:
+            conn.close()
+    print("Get stuff called")
+    print(username)
+    user=conn.execute("Select * from info where username=?",(username,)).fetchone()
+    print(user)
+    conn.close()
     return render_template(
-        "success.html",
-        isSuccess=isSuccess
+        'form.html',
+        user=user,
+        error=None
     )
-    
+
+
+@app.route("/allusers")
+def list_all_users():
+    conn=get_db()
+    users=conn.execute("Select * from info").fetchall()
+    conn.close()
+    return render_template(
+        "listusers.html",
+        users=users
+    )
+
 
 
 
--- static/script2.js
+++ static/script2.js
@@ -5,6 +5,13 @@ const passwordCheck=document.getElementById('password');
 const conPassword=document.getElementById('confirm-password');
 const resultDiv=document.getElementById('endResult')
 const ageCheck=document.getElementById('age')
+const searchBtn=document.getElementById('search_btn')
+usernameCheck.removeAttribute('readonly')
+if (localStorage.getItem('readonlyFlag')==="true"){
+    usernameCheck.setAttribute('readonly','')
+} else{
+    usernameCheck.removeAttribute('readonly')
+}
 Form.addEventListener('submit',validator);
 function showError(input,msg){
     const formElement=input.parentElement;
@@ -18,7 +25,6 @@ function showsuccess(input){
 }
 
 
-
 function validator(e){
     e.preventDefault();
     resultDiv.textContent=""
@@ -27,33 +33,32 @@ function validator(e){
     let pass=false
     let user=false
     let ageFlag=false
+    let isUpdate=localStorage.getItem("updateTrue")
     user=usernameValid((usernameCheck),3,25)
     em=emailCheckings(emailCheck)
     pass=passwordValid(passwordCheck,6,25)
     con=cValid((conPassword))
     ageFlag=validAge(ageCheck,10,80)
 
-
-    if (user && em && pass && con && ageFlag ){
-    debugger
+    if (user && em && pass && con && ageFlag && localStorage.getItem("readonlyFlag")!=="true"){
     username=usernameCheck.value;
     eName=emailCheck.value
     passName=passwordCheck.value
     cpassName=conPassword.value
     ageName=ageCheck.value
-        fetch("/api/reg?uName="+encodeURIComponent(username)+"&eName="+encodeURIComponent(eName)+"&passName="+encodeURIComponent(passName)+"&cpassName="+encodeURIComponent(cpassName)+"&ageName="+encodeURIComponent(ageName))
+        fetch("/api/reg?uName="+encodeURIComponent(username)+"&eName="+encodeURIComponent(eName)+"&passName="+encodeURIComponent(passName)+"&ageName="+encodeURIComponent(ageName))
             .then(function(response){
                 return response.json()
             })
             .then(function(data){
                 resultDiv.textContent=data.regMsg;
-                if (data.regMsg===`Your Registration has been ${username}, ${eName}, ${passName}, ${cpassName}, ${ageName}` & !data.error){
+                if (data.regMsg===`Your Registration has been ${username}, ${eName}, ${passName}, ${ageName}` & !data.error){
                     let isSuccess="Successfully registered. Saved to the database"
                     // fetch("/success?isSuccess="+encodeURIComponent(isSuccess))
                     //     .then(function(response){
                     //         return response
                     //     })
-                    window.location.href="/success?isSuccess="+encodeURIComponent(isSuccess)
+                    window.location.href="/allusers"
                 }
                 else{
                     let isSuccess=`Regestration has failed on Primary Key constraint on the username:  ${username} `
@@ -66,10 +71,59 @@ function validator(e){
 
                 }
             })
+    }
+    else if(user && em && pass && con && ageFlag && localStorage.getItem("readonlyFlag")==="true"){
+        // 1. Package the data you want to send in the POST request body
+    const payload = {
+        email: emailCheck.value,
+        age: ageCheck.value,
+        password: passwordCheck.value,
+        // Add any other variables you need to send to the backend here
+    };
 
-}
-
-
+    // 2. Make the POST request to your Flask route
+    fetch(`/update/${username.value}`, {
+        method: 'POST', // Explicitly setting the method to POST
+        headers: {
+            'Content-Type': 'application/json' // Telling Flask to expect JSON data
+        },
+        body: JSON.stringify(payload) // Converting the JS object to a JSON string
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json(); 
+    })
+    .then(data=>{
+        if (data.happy==="success"){
+            window.location.href="/allusers"
+        }
+    })
+    .then(data => {
+        console.log('Server response:', data);
+    })
+    .catch(error => {
+        console.error('Error during POST request:', error);
+    });
+    }
+    // else if(user && em && pass && con && ageFlag && isUpdate==="true"){
+    //     uName=usernameCheck.value;
+    //     eName=emailCheck.value
+    //     passName=passwordCheck.value
+    //     cpassName=conPassword.value
+    //     ageName=ageCheck.value
+    //     exUsername=localStorage.getItem("usernameHappy")
+    //     fetch("/update?uName="+encodeURIComponent(uName)+"&eName="+encodeURIComponent(eName)+"&ageName="+encodeURIComponent(ageName)+"&passName="+encodeURIComponent(passName)+"&cpassName="+encodeURIComponent(cpassName)+"&ex_username="+encodeURIComponent(exUsername))
+    //         .then(function(response){
+    //             return response.json()
+    //         })
+    //         .then(function(data){
+    //             resultDiv.textContent=data.msg
+    //         })
+    //     isUpdate=false
+    //     localStorage.setItem("updateTrue","false")
+    // }
 }
 function emailCheckings(emailVal){
     emailVal=emailVal.value
